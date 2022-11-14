85c7fee02bfa04101a92a07bd90814da60c3a83e
donovanwardd <wardd319@newschool.edu>
2022-11-14 14:45:24 -0500

    added quote button and transition for entires at the bottom

---
+++ entry-9/script.js
@@ -0,0 +1,28 @@
+
+let button = document.querySelector('#example');
+
+button.onclick = () => {
+	alert('"They who dream by day are cognizant of many things which escape those who dream only by night". -Edgar Allan Poe');
+};
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++){
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+
+
+}
