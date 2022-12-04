e8f6b7f6675d065d53bd764b36d2c8edd521b811
donovanwardd <wardd319@newschool.edu>
2022-12-04 16:31:59 -0500

    entry 8 in entry 11 style

--- entry-8/script.js
+++ entry-8/script.js
@@ -2,7 +2,7 @@
 let button = document.querySelector('#example');
 
 button.onclick = () => {
-	alert('"The only way to make sense out of change is to plunge into it, move with it, and join the dance." -Alan Watts');
+	alert('"I dreamed I was a butterfly, flitting around in the sky; then I awoke. Now I wonder: Am I a man who dreamt of being a butterfly, or am I a butterfly dreaming that I am a man?" -Zhuangzi');
 };
 
 window.addEventListener('scroll', reveal);
